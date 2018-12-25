461597d4ffc0ce5c5f9cc72cd687b427f3cc6f3c
ambatiramireddy <ambati.ramireddy@gmail.com>
2018-12-25 14:38:18 +0530

    Add files via upload

---
+++ ISqlHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AddAppAPI.Helpers
+{
+    public interface ISqlHelper<T>
+    {
+        Task<List<T>> SelectAllAsync();
+
+        Task<Dictionary<I, N>> SelectIdNamePairsAsync<I, N>();
+
+        Task<T> SelectOneAsync(Dictionary<string, object> parameters);
+
+        Task<bool> InsertAsync(T item);
+
+        Task<bool> UpdateAsync(T item);
+
+        Task<bool> DeleteAsync(Dictionary<string, object> parameters);
+    }
+}
